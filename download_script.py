99b4adf86d9b7ad53b4c541e1ed08ff6b2ea6c7e
Alex Thompson <alexthompson1991@hotmail.com>
2019-04-29 22:48:33 -0700

    Put everything in a Downloader class

--- download_script.py
+++ download_script.py
@@ -1,67 +1,9 @@
-from binance.client import Client
-import pandas as pd
-import json
-
-def configure():
-    with open('config.json') as f:
-        config = json.load(f)
-    return config
-
-def get_ohlc(client, **kwargs):
-    history = client.get_historical_klines(
-        kwargs['symbol'],
-        kwargs['granularity'],
-        kwargs['start'],
-        kwargs['end']
-    )
-
-    df = pd.DataFrame({
-        'time': [t[0] for t in history],
-        'open': [t[1] for t in history],
-        'high': [t[2] for t in history],
-        'low': [t[3] for t in history],
-        'close': [t[4] for t in history],
-        'volume': [t[5] for t in history]
-    })
-    return df
-
-def make_trading_pairs(tickers):
-    get_pairs = []
-    for i in tickers:
-        for j in tickers:
-            if i != j:
-                get_pairs.append(i + j)
-    return get_pairs
- 
+from downloader import Downloader
 
 if __name__ == "__main__":
     print("Configuring")
-    is_debug = True
-    config = configure()
-    client = Client(
-        config['public_key'],
-        config['private_key']
-    )
-    if is_debug:
-        sub_config = config['debug']
-    else:
-        sub_config = config['prod']
-    exchange_info = client.get_exchange_info()
-    exchange_symbols = exchange_info["symbols"]
-    print("Getting symbols")
-    get_symbols = sub_config["symbols"]
-    get_pairs = make_trading_pairs(get_symbols)
-    for symbol in exchange_symbols:
-        pair = symbol['baseAsset'] + symbol['quoteAsset']
-        if pair in get_pairs:
-            print(pair)
-            ohlc = get_ohlc(
-                client,
-                symbol=pair,
-                start=sub_config['start'],
-                end=sub_config['end'],
-                granularity=sub_config['granularity']
-            )
-            filename = 'data/' + pair + '.csv'
-            ohlc.to_csv(filename)
-
+    downloader = Downloader(True)
+    downloader.configure()
+    downloader.make_client()
+    downloader.fetch()
+    downloader.to_csv('data')
---
+++ downloader.py
@@ -0,0 +1,75 @@
+from binance.client import Client
+import pandas as pd
+import json
+import os
+
+def make_trading_pairs(tickers):
+    get_pairs = []
+    for i in tickers:
+        for j in tickers:
+            if i != j:
+                get_pairs.append(i + j)
+    return get_pairs
+
+class Downloader(object):
+
+    def __init__(self, debug):
+        self.debug = debug
+        self.config = None
+        self.env = None
+        self.client = None
+        self.data = {}
+ 
+    def configure(self):
+        with open('config.json') as f:
+            self.config = json.load(f)
+        if self.debug:
+            self.env = self.config['debug']
+        else:
+            self.env = self.config['prod']
+ 
+    def make_client(self):
+        self.client = Client(
+            self.config['public_key'],
+            self.config['private_key']
+        )
+
+    def fetch(self):
+        exchange_info = self.client.get_exchange_info()
+        exchange_symbols = exchange_info["symbols"]
+        get_symbols = self.env["symbols"]
+        get_pairs = make_trading_pairs(get_symbols)
+        for symbol in exchange_symbols:
+            pair = symbol['baseAsset'] + symbol['quoteAsset']
+            if pair in get_pairs:
+                print(pair)
+                ohlc = self._get_ohlc(pair)
+                self.data[pair] = ohlc
+
+    def _get_ohlc(self, symbol):
+        history = self.client.get_historical_klines(
+            symbol,
+            self.env['granularity'],
+            self.env['start'],
+            self.env['end']
+        )
+
+        df = pd.DataFrame({
+            'time': [t[0] for t in history],
+            'open': [t[1] for t in history],
+            'high': [t[2] for t in history],
+            'low': [t[3] for t in history],
+            'close': [t[4] for t in history],
+            'volume': [t[5] for t in history]
+        })
+        return df
+
+
+    def to_csv(self, target_dir):
+        dirs = os.listdir()
+        if target_dir not in dirs:
+            os.mkdir(target_dir)
+        for k in self.data.keys():
+            self.data[k].to_csv(target_dir + '/' + k + '.csv')
+
+
