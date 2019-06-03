2cc3c60c5d2a43b8aa02cc1e2b390439545fcf9c
Alex Thompson <alexthompson1991@hotmail.com>
2019-06-03 10:20:48 -0700

    Altered rolling analysis

--- core/rolling_analysis.py
+++ core/rolling_analysis.py
@@ -21,19 +21,23 @@ class RollingAnalyzer:
     def __init__(self, prices, m):
         self.prices = prices
         self.returns = pd.DataFrame(prices).diff()
+        self.assets = prices.columns
         self.n = prices.shape[0]
         self.m = m
         self.rolled_dfs = None
-        self.regressions = {}
+        self.rolled_returns = None
+        self.rolled_regressions = {}
+        self.rolled_returns_stats = {}
         self.pairs = []
     
     def make_dfs(self):
         roll_range = range(0, self.n - self.m)
         self.rolled_dfs = [pd.DataFrame(self.prices.iloc[i:(i+self.m), :]) for i in roll_range]
+        self.rolled_returns = [pd.DataFrame(self.returns.iloc[i:(i+self.m), :]) for i in roll_range]
     
     def run_regressions(self, asset_1, asset_2):
         code = asset_1 + '_' + asset_2
-        self.regressions[code] = []
+        self.rolled_regressions[code] = []
         for df in self.rolled_dfs:
             date_range = df.index[0], df.index[df.shape[0]-1]
             x = df[asset_1].values.reshape(-1, 1)
@@ -45,7 +49,7 @@ class RollingAnalyzer:
                 'regression': linear_regression,
                 'residuals': residuals,
             }
-            self.regressions[code].append(reg_dict)
+            self.rolled_regressions[code].append(reg_dict)
         
     def asset_loop(self):
         for i in range(0, self.prices.shape[1]):
@@ -65,10 +69,10 @@ class RollingAnalyzer:
     
     def calc_dickey_fuller(self, asset_1, asset_2):
         code = asset_1 + '_' + asset_2
-        if code not in self.regressions.keys():
+        if code not in self.rolled_regressions.keys():
             print('Assets not regressed yet')
         else:
-            for regression in self.regressions[code]:
+            for regression in self.rolled_regressions[code]:
                 regression['dickey_fuller'] = ts.adfuller(regression['residuals'])
                 
     def calc_all_dickey_fullers(self):
@@ -78,14 +82,62 @@ class RollingAnalyzer:
             for pair in self.pairs:
                 print(pair)
                 self.calc_dickey_fuller(pair[0], pair[1])
+
+    def calc_returns_stats(self, asset):
+        self.rolled_returns_stats[asset] = []
+        for df in self.rolled_returns:
+            date_range = df.index[0], df.index[df.shape[0]-1]
+            returns = df[asset]
+            
+            mean_return = np.mean(returns)
+            sd_return = np.std(returns)
+            sharpe = mean_return / sd_return
+
+            stats_dict = {
+                'date_range': date_range,
+                'mean_return': mean_return,
+                'volatility': sd_return,
+                'sharpe': sharpe
+            }
+
+            self.rolled_returns_stats[asset].append(stats_dict)
+
+    def calc_all_returns_stats(self):
+        for asset in self.assets:
+            self.calc_returns_stats(asset)
                 
-    def export_df(self):
+    def export_dickey_fuller_df(self):
         dickey_fullers = {}
-        date_index = []
-        for k, v in self.regressions.items():
-            dickey_fullers[k] = v['dickey_fuller']
-            date_index.append(v['date_range'][0])
-        out_df = pd.from_dict(dickey_fuller)
-        out_df.index = date_index
-        return(out_df)
+
+        for key in self.rolled_regressions.keys():
+            regressions = self.rolled_regressions[key]
+            if 'dickey_fuller' in regressions[0].keys():
+                dickey_fuller = [t['dickey_fuller'][1] for t in regressions]
+                date_range = [i['date_range'][0] for i in regressions]
+                dickey_fullers[key] = pd.Series(dickey_fuller, index=date_range)
+
+        out_df = pd.DataFrame.from_dict(dickey_fullers)
+        return out_df
+
+    def export_betas_df(self):
+        betas = {}
+        for key in self.rolled_regressions.keys():
+            regressions = self.rolled_regressions[key]
+            betas_temp = np.array([r['regression'].coef_[0] for r in regressions])
+            date_range = [i['date_range'][0] for i in regressions]
+            betas[key] = pd.Series(betas_temp, index = date_range)
+        out_df = pd.DataFrame.from_dict(betas)
+        return out_df
+
+    def export_returns_stats_df(self, stat='mean_return'):
+        mean_returns = {}
+        for key in self.rolled_returns_stats.keys():
+            stats = self.rolled_returns_stats[key]
+            mean_return_dict = {i['date_range'][0]: i[stat] for i in stats}
+            mean_returns[key] = pd.Series(mean_return_dict)
+        out_df = pd.DataFrame.from_dict(mean_returns)
+        return out_df
+
         
+
+
--- full_analysis_script.py
+++ full_analysis_script.py
@@ -22,23 +22,35 @@ if __name__ == "__main__":
   print("filtering to USDT")
   pairs = get_pairs(good_dfs)
   closes_usdt = closes[pairs['USD']]
-  
+
   print("calculating log prices")
   log_df = calc_log_prices(closes_usdt)
+  filtered_log_df = log_df['2019-01-01':'2019-02-01']
 
   print("calculating returns")
   returns = calc_returns(closes_usdt)
   print("-------------------")
   print("Now rolling analysis")
-  rolling_analyzer = RollingAnalyzer(log_df, 100)
+  rolling_analyzer = RollingAnalyzer(filtered_log_df, 100)
   print("Make the dfs")
   rolling_analyzer.make_dfs()
   rolling_analyzer.asset_loop()
   print("Running all the regressions")
-  rolling_analyzer.run_all_regressions()
-  
-  # print("Calculating dickey fullers")
-  # rolling_analyzer.calc_all_dickey_fullers()
+  # rolling_analyzer.run_all_regressions()
+  rolling_analyzer.run_regressions('BTCUSDT', 'ETHUSDT')
+  rolling_analyzer.run_regressions('BTCUSDT', 'LTCUSDT')
+  rolling_analyzer.calc_returns_stats('BTCUSDT')
 
+  print("Calculating dickey fullers")
+  # rolling_analyzer.calc_dickey_fuller('BTCUSDT', 'ETHUSDT')
+  # rolling_analyzer.calc_dickey_fuller('BTCUSDT', 'LTCUSDT')
+
+  print("Exporting")
+  dickey_fuller_df = rolling_analyzer.export_dickey_fuller_df()
+  print(dickey_fuller_df.head())
+  betas_df = rolling_analyzer.export_betas_df()
+  print(betas_df.head())
+
+  # rolling_analyzer.calc_all_dickey_fullers()
 
 
